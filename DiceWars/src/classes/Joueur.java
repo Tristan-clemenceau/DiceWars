29b8a6fb2aa2222b36e75d01d21162d59ed5f256
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-01-06 19:37:03 +0100

    Question 1

--- DiceWars/src/classes/Joueur.java
+++ DiceWars/src/classes/Joueur.java
@@ -1,5 +1,23 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Joueur {
+	private final int id;
+	
+	public Joueur(int id) {
+		this.id = id;
+	}
+
+	public void jouer() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pour terminer votre tour entrez (\"q\")");
+		String choix = sc.nextLine();
+		
+		while (!choix.toLowerCase().equals("q")) {
+			System.out.println("Pour terminer votre tour entrez (\"q\")");
+			choix = sc.nextLine();
+		}
+	}
 
 }
--- DiceWars/src/tests/TestMain.java
+++ DiceWars/src/tests/TestMain.java
@@ -1,10 +1,13 @@
 package tests;
 
+import classes.Joueur;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Joueur j1 = new Joueur(1);
+		j1.jouer();
 	}
 
 }
